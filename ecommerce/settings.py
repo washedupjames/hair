e121148c5f6d5ab16e974c2121be3736c453de82
James <parkerman23@gmail.com>
2025-02-08 01:54:53 +0000

    Beta v3.3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -101,13 +101,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+
 
 
 # Password validation
@@ -173,7 +175,7 @@ HOST_EMAIL = env('EMAIL_HOST_USER')
 
 
 
-'''
+
 
 
 
@@ -226,4 +228,4 @@ DATABASES = {
     }
 }
 
-'''
+
