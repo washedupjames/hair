2e8af03726448559e4f3064229cb8f57d80e2d57
James <parkerman23@gmail.com>
2025-02-11 23:44:04 +0000

    live 4.2

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,11 +26,19 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'tees-naturals.online',
+    'www.tees-naturals.online', 
 
-#CSRF_TRUSTED_ORIGINS = ['']
+    ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://www.tees-naturals.online',
+    'https://tees-naturals.online',
+
+]
 # Application definition
 
 INSTALLED_APPS = [
