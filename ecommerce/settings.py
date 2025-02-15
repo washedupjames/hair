89d7dbcb3d497d0f67597ef0b593552bf2891dd3
James <parkerman23@gmail.com>
2025-02-15 00:17:04 +0000

    Live V4

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,6 +26,190 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'tees-naturals.online',
+    'www.tees-naturals.online', 
+    '*'
+
+    ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://www.tees-naturals.online',
+    'https://tees-naturals.online',
+
+]
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'store', # Django app
+
+    'cart', # Django app
+
+    'account', # Django app
+
+    'payment', # Django app
+    
+    'mathfilters',
+
+    'crispy_forms', # Crispy forms
+
+    'django_countries',
+
+    'storages',
+
+]
+
+# To un-block PayPal popups - NB!
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY='same-origin-allow-popups'
+# Crispy forms
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+MIDDLEWARE = [ 
+
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'ecommerce.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'store.views.categories', # Updated
+                'cart.context_processors.cart',
+
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'ecommerce.wsgi.application'
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+# Internationalization
+# https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'static/media'
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Email configuration settings:
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+# Be sure to read the guide in the resources folder of this lecture (SETUP THE EMAIL BACKEND)
+
+EMAIL_HOST_USER = env('EMAIL_HOST_USER') # - Enter your GMAIL address # The host email that sends password reset emails
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD') # - Enter your app password 
+HOST_EMAIL = env('EMAIL_HOST_USER')
+
+
+
+import os
+
+from pathlib import Path
+import environ
+env = environ.Env()
+
+import environ
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Assuming your .env file is in the right place
+PAYPAL_CLIENT_ID = os.getenv('PAYPAL_CLIENT_ID')
+SANDBOX_PAYPAL_CLIENT_ID = os.getenv('SANDBOX_PAYPAL_CLIENT_ID')
+env = environ.Env()
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    env.read_env(dotenv_path)
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent  # Adjusted for correct path
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = [
@@ -237,4 +421,64 @@ DATABASES = {
     }
 }
 
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+
+
+
+
+
+
+# AWS configuration
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID') # - Enter your AWS ACCESS KEY ID HERE
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY') # - Enter your AWS SECRET ACCESS KEY HERE
+# Amazon S3 Integration
+
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME') # - Enter your S3 bucket name HERE
+
+# Django 4.2 > Storage configuration for S3
+
+STORAGES = {
+    
+    # Media file (image) management
+
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
+    
+    # CSS and JS file management
+
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+        
+    },
+    
+}
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_FILE_OVERWRITE = False
+
+# RDS (Database) configuration settings:
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': env('DB_NAME'), # Enter your Database name HERE
+
+        'USER': env('DBUSER'), # Enter your Database username HERE
+
+        'PASSWORD': env('DBPASSWORD'), # Enter your Database password HERE
+
+        'HOST': env('DB_HOST'), # Enter your Database host/endpoint HERE
+
+        'PORT': '5432',
+    }
+}
 
